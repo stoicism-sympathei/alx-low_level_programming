70eb32ff0250eda5e99eb8b7e4863d074a71ad4f
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-08 18:12:48 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, fllowed by a new line.
+ *
+ * Reoturn: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
