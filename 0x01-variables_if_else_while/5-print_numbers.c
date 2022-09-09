f5f95c5096ca2fb9aa5c2d7f434e9a8deafd4ca9
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-09 18:45:54 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints numbers from 0 to 9
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+		printf("%d", n++);
+	putchar('\n');
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
