4036298b7f88efc0ada3f4f1db13269b300c3178
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-13 20:13:34 +0300

    Debuggme

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,5 @@
+void positive_or_negative(int i);
+int largest_number(int a, int b, intc);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
