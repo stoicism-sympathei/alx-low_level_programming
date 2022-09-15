ddfa518a4019aff86b48d2717e61a8239f710007
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-15 14:16:11 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,13 +21,15 @@ int main(void)
 
 		else if ((num % 5) == 0)
 			printf("Buzz");
+
 		else
 			printf("%d", num);
+
 		if (num == 100)
 			continue;
 		printf(" ");
 	}
-	
+
 	printf("\n");
 
 	return (0);
