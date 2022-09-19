186d503a358ad63c9de6e104dc62a782f202ecd2
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-19 12:04:46 +0300

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @s: the string
+ *
+ * Return: the length of the s string
+ */
+int _strlen(char *S)
+{
+	int 1 = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
