2d43cf85f73d070b3940116614d62d984874ef00
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-19 12:39:40 +0300

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,11 +4,12 @@
  * _puts - prints a string, followed by a newline, to sdtout
  * @str: the string to be printed
  */
+
 void _puts(char *str)
 {
 	int i = 0;
 
 	while (str[i]);
-	 _putchar(str[i++]);
+	        _putchar(str[i++]);
 	_putchar('\n');
 }
