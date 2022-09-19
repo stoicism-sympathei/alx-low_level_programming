1d83a71433162131dd1672ccbcf8c98c42c696e4
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-19 14:56:49 +0300

    Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string into an integer
+ * @s: the string to be converted
+ *
+ * Return: the value of the int
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 0, res = 0;
+
+	while (s[i] && (!(s[i] >= '0') || !(s[i] <= '9')))
+	{
+		if (s[i] == '-')
+			sign++;
+		i++;
+	}
+	sign = (sign % 2) ? -1 : 1;
+	while (s[i] && ((s[i] >= '0') && (s[i] <= '9')))
+		res = res * 10 + (s[i++] - '0' * sign;
+	return (res);
+}
