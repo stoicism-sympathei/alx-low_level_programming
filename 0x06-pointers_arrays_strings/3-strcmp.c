44988e3e75e1fcbe6920041ab58f674fdb09ab60
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-21 20:06:20 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: returns an integer less than, equal to, or greater than zero
- * if s1  is found, respectively, to be less than, to match, or be greater than s2
+ * _strcmp - compare
+ * @s1 : pointerto char params
+ * @s2 : pointer to char params
+ * Return: *dest
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
@@ -20,5 +19,6 @@ int _strcmp(char *s1, char *s2)
 		i++;
 	}
 	R = s1[i] - s2[i];
+
 	return (R);
 }
