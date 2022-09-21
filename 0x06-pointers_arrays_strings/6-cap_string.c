a83231f7075aac5f13cdfb3a42868f80abee4d75
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-21 19:04:15 +0300

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,34 +6,34 @@
  *
  * Return: 1 if yes, 0 if no
  */
-char *cap_string(char *str)
+
+int		issep(char c)
 {
-	int index = 0;
+	int i = 0;
+	char seps[14] = " \t\n,;.!?\"(){}";
 
+	while (c != seps[i])
+		i++;
+	return ((i <= 12));
+}
 
-	while (str[index])
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: the string
+ *
+ * Return: a pointer to the string
+ */
+
+char	*cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
+		if (i == 0 || issep(s[i - 1]))
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+		i++;
 	}
-
-	return (str);
+	return (s);
 }
