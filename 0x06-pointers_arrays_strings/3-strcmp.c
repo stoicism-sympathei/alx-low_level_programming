94baaf786eaf55932e3a7f8426daf79790181d82
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-21 19:16:28 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-
-	while (s1[i] == s2[i] && s1[i] && s2[i])
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	return (s1[i] - s2[i]);
+
+	return (*s1 - *s2);
 }
