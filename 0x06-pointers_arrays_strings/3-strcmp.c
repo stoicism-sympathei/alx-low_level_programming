d08b6887a3c0fd8602caf12e2055a3dbbaffba95
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-21 19:13:12 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,10 @@
  * return: return an integer less than, equalto, or greater than zero
  * if s1 is found, respectively, to be less than, to match, or be greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+
 
 	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
