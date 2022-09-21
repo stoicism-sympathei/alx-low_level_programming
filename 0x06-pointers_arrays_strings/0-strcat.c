194feee0114e250867ef28fedb54f988839c846e
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-21 11:30:06 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,14 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i = 0, j = 0;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dast);
+	while (dest[i])
+		i++;
+	while (j < n && src[j])
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest);
 }
