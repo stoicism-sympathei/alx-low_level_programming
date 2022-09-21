3ce02a3730863517dce3add7de22d41b27e4f920
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-21 19:52:28 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s2: second string
  *
  * Return: returns an integer less than, equal to, or greater than zero
- *if s1  is found, respectively, to be less than, to match, or be greater than s2
+ * if s1  is found, respectively, to be less than, to match, or be greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
