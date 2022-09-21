8f79e6049b6ec86fed02d514de45cd9f4a9bf4bf
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-21 19:31:52 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,15 +5,14 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: returns an integer less than, equal to, or greater than zero if s1
- * is found, respectively, to be less than, to match, or be greater than s2
+ * Return: returns an integer less than, equal to, or greater than zero
+ *if s1  is found, respectively, to be less than, to match, or be greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
 	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
-	retun(s1[i] - s2[i]);
-}r 
+	return (s1[i] - s2[i]);
+}
