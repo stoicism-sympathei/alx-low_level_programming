58d60e3895bd48988eca85ab7f0c75c8380c5f1c
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-26 13:20:01 +0300

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: the string to look inside
+ * @c: the character to find
+ *
+ * Return: a pointer to the first occurence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (s[i] == c)
+		return (s + i);
+	return (NULL);
+}
