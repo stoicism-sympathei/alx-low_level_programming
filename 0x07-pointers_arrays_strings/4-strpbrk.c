7a3619d7d78554c3082eb87c37565208ccfaee1c
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-26 13:49:47 +0300

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: the string to look inside
+ * @c: the character to find
+ *
+ * Return: a pointer to the first occurence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (s[i] == c)
+		return (s + i);
+	return (NULL);
+}
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: sting to search inside
+ * @accept: chars to be accepted
+ *
+ * Return: pointer to the byte in s that matches one of the bytes or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+	{
+		if (_strchr(accept, s[i]))
+			break;
+		i++;
+	}
+	return (s[i] ? s + i : NULL);
+}
