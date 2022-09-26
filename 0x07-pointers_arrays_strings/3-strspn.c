5911c0a8ec1477887e8a7993d9986f95c0147778
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-26 13:43:47 +0300

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,6 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
- *
- * @s: the string to look inside
- * @c: the character to find
- *
- * Return: a pointer to the first occurence of c in s, or NULL if not found
- */
-
-char *_strchr(char *s, char c)
-{
-	unsigned int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] == c)
-			return (s + i);
-		i++;
-	}
-	if (s[i] == c)
-		return (s + i);
-	return (NULL);
-}
-
-/**
  * _strspn - gets the length of a prefix substring
  *
  * @s: the string
