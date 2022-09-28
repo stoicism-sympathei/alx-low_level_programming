2215e29cf75a4afeb40b1b977144d053f4ff8780
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-28 11:46:35 +0300

    Why is it so important to dream?

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string, followed by a newline
+ * @s: the string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
