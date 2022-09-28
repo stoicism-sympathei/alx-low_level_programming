d586095601eb51baba476b3274de837febe1433d
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-28 11:54:56 +0300

    Dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to measure
+ *
+ * Return: the length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
+	
