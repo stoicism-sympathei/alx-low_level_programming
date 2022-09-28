cb1f04cc31438bf18587d99843e05e634a3c391c
stoicism-sympathei <mogesanonymous@gmail.com>
2022-09-28 12:34:42 +0300

    header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,11 @@
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
