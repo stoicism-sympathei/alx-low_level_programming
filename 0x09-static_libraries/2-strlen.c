1267456b598c3a9f9614f4b04120c46b4a527ccc
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-02 11:30:56 +0300

    C files

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include  "main.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @s: the string
+ *
+ * Return: the length of the s string
+ */
+int	_strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
