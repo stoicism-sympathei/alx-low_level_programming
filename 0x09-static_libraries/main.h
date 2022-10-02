8d163c523180179ecd3f5ae32151c2bc8e3d2a53
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-02 13:10:59 +0300

    static

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,25 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int _isupper(ic);
-int digit(int c);
-int _strlen(chr *s);nt _isa
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char);
+int _putchar(char);
+int _islower(int);
+int _isalpha(int);
+int _abs(int);
+int _isupper(int);
+int _isdigit(int);
+int _strlen(char *);
+void _puts(char *);
+char *_strcpy(char *, char *);
+int _atoi(char *);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
 
 #endif
