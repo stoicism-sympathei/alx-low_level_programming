64cf52178fe2cd21bc8e241de33e3565e16023e3
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-03 12:15:59 +0300

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * exit_free - frees the whole array in case of a malloc error
+ *
+ * @array: the 2D array to free
+ * @nb: the number of int arrays inside array to free
+ */
+
+void exit_free(int **array, int nb)
+{
+	int i = 0;
+
+	while (i < nb)
+		free(array[i++]);
+	free(array);
+}
+
+/**
+ * alloc_grid - allocates a 2 dimensional array of integers
+ *
+ * @width: the width of the array
+ * @height: the height of the array
+ *
+ * Return: a pointer to the 2D array,
+ * or NULL if width or height is less or equal to 0 or on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i = 0, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	array = malloc(sizeof(int *) * height);
+	if (!array)
+		return (NULL);
+	while (i < height)
+	{
+		j = 0;
+		array[i] = malloc(sizeof(int) * width);
+		if (!array[i])
+		{
+			exit_free(array, i);
+			return (NULL);
+		}
+		while (j < width)
+			array[i][j++] = 0;
+		i++;
+	}
+	return (array);
+}
