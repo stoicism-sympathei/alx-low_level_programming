f8959f7e32e9ec848610365e3d52036418abb6f5
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-03 12:23:14 +0300

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional array of integers
+ *
+ * @grid: the grid to free
+ * @height: the hight of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+		free(grid[i++]);
+	free(grid);
+}
