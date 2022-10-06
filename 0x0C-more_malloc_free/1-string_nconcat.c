d1f737ed4462edd8e91f1c5df705d93119872813
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-06 14:51:56 +0300

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,55 +1,51 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of the string
- * @s: the string
- *
- * Return: the length of the s string
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-
-	if (s != NULL)
-		while ([i])
-			i++;
-	return (i);
-}
-
-/**
- * string_strnconcat - concatenates two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the maximum size in bytes of s2 to be concatenated to s1
- *
- * Return: a pointer to the new string, or NULL on failure
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int i = 0, j = 0;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
+	if (s1 == NULL)
+		s1 = "";
 
-	if (n >= (unsigned int)_strlen(s2))
-		n = (unsigned int)_strlen(s2);
-	str = malloc(sizeof(char) * (_strlen(s1) + n + 1));
-	if (!str)
-		return (NULL);
-	if (s1 != NULL)
-		while (s1[i])
-		{
-			str[i] = s1[i];
-			i++;
-		}
-	if (s2 != NULL)
-		while (s2[j] && j < n)
-		{
-			str[i + j] = s2[j];
-			j++;
-		}
-	str[i + j] = '\0';
-	return (str);
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
