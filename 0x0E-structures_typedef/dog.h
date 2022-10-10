9d0a8bd24d66d31a6efe7cc576f9951274cc0466
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-10 13:16:08 +0300

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,16 +11,21 @@
  * @age: the age of the dog
  * @owner: the owner of the dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
-void int_dog(struct dog *, char *, float, char *);
-void print_dog(struct dog *);
-dog_t *new_dog(char *, float, char *);
-void free_dog(dog_t);
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
