8aac8e69eba98274d327951fd5820af726601462
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-10 13:36:43 +0300

    How many legs does a dog have?

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,17 @@
 #include "dog.h"
 
 /**
- * free_dog - frees dog 
+ * free_dog - frees dog
  *
  * @d: the dog structure
  */
+
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
 		free(d->owner);
-		free(d->nmae);
+		free(d->name);
 		free(d);
 	}
 }
