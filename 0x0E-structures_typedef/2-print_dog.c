abb4155c8a1920e86e2f0eb02a51e5694cea26af
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-10 13:41:47 +0300

    things won't be too bad

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,15 +1,16 @@
-#includ "dog.h"
+#include "dog.h"
 
 /**
  * print_dog - prints the structure dog
  *
- * @d: the dog structure to print
+ * @d: the dog structure to prin
  */
-void print_dog(struct dog *d)
+
+void	print_dog(struct dog *d)
 {
 	if (d)
-	{
-		printf("Name: %S\n", (d->name ? d->name : "(nil)"));
+t	{
+		printf("Name: %s\n", (d->name ? d->name : "(nil)"));
 		printf("Age: %0.6f\n", (d->age));
 		printf("Owner: %s\n", (d->owner ? d->owner : "(nil)"));
 	}
