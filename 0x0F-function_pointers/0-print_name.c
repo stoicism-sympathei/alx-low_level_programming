6a621c36782eae76de51d1b2ffe6bb9bfd26945e
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-12 12:38:24 +0300

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name given to parameter
+ *
+ * @name: the name to print
+ * @f: the function to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		exit(98);
+	(*f)(name);
+}
