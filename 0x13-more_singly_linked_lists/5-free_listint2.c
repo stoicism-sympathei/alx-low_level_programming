7cc4fe08b3729a6a71c5bc31766e2d6c2783acbf
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-24 18:28:20 +0300

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a listint_t list and sets the head to null
+ * free_listint2 - frees a listint_t list ans sets the head to NULL
  *
- * *head: the listint_t argument (head)
+ * @head: the listint_t argument (head)
  */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *node = NULL;
