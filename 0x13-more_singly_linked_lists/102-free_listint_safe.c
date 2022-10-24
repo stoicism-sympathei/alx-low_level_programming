42fa6e5ff17d071bcdaa3d45898071bd9ae24eaf
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-24 15:06:54 +0300

    Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t linked list
+ *
+ * @head: the listint_t argument (head)
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t free_listint_safe(listint_t **head)
+{
+	size_t count = 0, i = 0;
+	listint_t *begin = *head, *tmp;
+
+	if (*head)
+	{
+		while (*head && interval(begin, *head, count))
+		{
+			tmp = (*head)->next;
+			*head = tmp;
+
+			count++;
+		}
+
+		*head = begin;
+		while (i < count)
+		{
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
+			i++;
+		}
+
+		if (*head)
+			*head = NULL;
+	}
+	return (count);
+}
