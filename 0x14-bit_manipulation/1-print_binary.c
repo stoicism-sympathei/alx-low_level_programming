5cf8039216788c3a8085d0c4725494e144f87a13
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-27 11:59:10 +0300

    reperesent

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary_rec - the recursive function to handle the print binary
+ *
+ * @n: the number to print in binary
+ */
+
+void print_binary_rec(unsigned long int n)
+{
+	if (n != 0)
+	{
+		print_binary_rec(n >> 1);
+		_putchar((n & 1) ? '1' : '0');
+	}
+}
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: the number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+		print_binary_rec(n);
+}
