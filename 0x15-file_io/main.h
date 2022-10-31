6861114c4431f3c9c20f981ed703ea2432d8c9fd
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-31 17:12:11 +0300

    header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,13 +3,13 @@
 
 #include <unistd.h>
 #include <fcntl.h>
-#include <sdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <elf.h>
 
 int _putchar(char c);
 
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t	read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
