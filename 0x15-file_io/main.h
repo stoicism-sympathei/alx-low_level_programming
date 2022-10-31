bfd4307c79a1b7857b9e00d506fbe4fca9a30537
stoicism-sympathei <mogesanonymous@gmail.com>
2022-10-31 11:46:04 +0300

    header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sdlib.h>
+#include <stdio.h>
+#include <elf.h>
+
+int _putchar(char c);
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */
